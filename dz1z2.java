43a70857dc459dce559e8573815c8b276b892340
KhabarovaND <N.D.Khabarova@gmail.com>
2023-03-26 19:34:20 +0300

    задача2

---
+++ dz1z2.java
@@ -0,0 +1,11 @@
+//import java.util.Random;
+
+//2. Посчитать и сохранить в n номер старшего значащего бита выпавшего числа
+
+public class programHighBit {
+    public static int significantBit(int i) {
+        int binLen = Integer.toBinaryString(i).length();
+        return binLen;
+
+    }
+  }

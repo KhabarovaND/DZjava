a59a96a34d9f37950656bf000b4988899df3ec6e
KhabarovaND <N.D.Khabarova@gmail.com>
2023-04-04 18:35:05 +0300

    задача1-4

---
+++ dz4z1-4.java
@@ -0,0 +1,52 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Program
+{
+    public static void main(String[] args) throws IOException
+    {
+        ArrayList<String> family = new ArrayList<>();
+        ArrayList<String> name = new ArrayList<>();
+        ArrayList<String> soname = new ArrayList<>();
+        ArrayList<Integer> age = new ArrayList<>();
+        ArrayList<Boolean> gender = new ArrayList<>();
+        LinkedList<Integer> index = new LinkedList<>();
+
+
+        String text = " ";
+        FileReader reader = new FileReader("db.sql");
+        while (reader. ready ()) {
+            text += (char) reader. read();
+        }
+        reader.close();
+        String [] str = text.split("\r\n");
+        for (int i = 1; i < str.length; i++){
+            String [] sb = str[i].split(" ");
+
+            family.add(sb[0] + " ");
+            name.add(sb[1].substring(0, 1) + ".");
+            soname.add(sb[2].substring(0, 1) + "." + " ");
+            age.add(Integer.valueOf(sb[3]));
+            gender.add(sb[4] == "M" ? true : false);
+            index.add(i);
+        }
+        
+   
+        for (int i = 0; i < index.size(); i++){
+            System.out.printf(family.get(i));
+            System.out.printf(name.get(i));
+            System.out.printf(soname.get(i));
+            System.out.printf(age.get(i).toString());
+            System.out.printf((gender.get(i) ? "M" : "Ж"));
+            System.out.println();
+        }
+
+           
+    }
+    
+}
+
+    
+
